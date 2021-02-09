37e439c3cb6a688a2be2b8bf62f78dde713ddd0c
Hassan <haslek2013@gmail.com>
2021-02-10 00:58:38 +0100

    Started socket service implementation

--- lib/service_locator.dart
+++ lib/service_locator.dart
@@ -4,4 +4,5 @@ import 'package:get_it/get_it.dart';
 GetIt locator = GetIt.instance;
 void setUpLocator(){
   locator.registerSingleton<SocketService>(SocketService(),signalsReady: true);
+  locator.registerSingleton<User>(User());
 }
--- lib/utils.dart
+++ lib/utils.dart
@@ -3,6 +3,38 @@ import 'dart:async';
 import "package:socket_io_client/socket_io_client.dart";
 import "package:socket_io_client/socket_io_client.dart" as IO;
 
+class User{
+   String displayName;
+   String userId;
+   String email;
+   String phone;
+   String firstName;
+   String lastName;
+   User({this.displayName,this.email,this.firstName,this.lastName,this.phone,this.userId});
+
+   Map<String, dynamic> toJson(){
+     return {
+       "display_name":displayName,
+       "user_id":userId,
+       "email":email,
+       "phone":phone,
+       "first_name":firstName,
+       "last_name":lastName
+     };
+   }
+
+   factory User.fromJson(Map<String,dynamic> json) {
+     return User(
+       displayName: json['display_name'],
+       email: json['email'],
+       phone: json['phone'],
+       userId: json['id'],
+       firstName: json['first_name'],
+       lastName: json['last_name']
+     );
+   }
+}
+
 class SocketService{
   final _socketResponse = StreamController<String>();
   IO.Socket socket;
@@ -13,10 +45,14 @@ class SocketService{
   void dispose(){
     _socketResponse.close();
   }
+  Map<String,dynamic> userDetails = {
+    "user_id":""
+  };
   void connectAndListen(){
     this.socket = IO.io("http://localhost:3000",OptionBuilder().setTransports(['websocket']).build());
     this.socket.onConnect((_){
       print("Connected");
+      this.socket.emit("online",);
     });
     this.socket.on('new message',(data)=>this.addResponse);
     this.socket.onDisconnect((_)=>print("Disconnected"));
